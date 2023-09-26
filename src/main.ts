022407366aef239c221d563801523fc1fbca9269
becauro <michelbecauro@gmail.com>
2023-09-26 00:53:14 -0300

    FEAT: Restructure the src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,20 @@ import readLine from 'readline-sync';
 // import { poolcreator as connection } from './models/connection'; // testando
 import BookModel, { Book } from './models/Book'; 
 
-const main = async () => {
+const bookModel = new BookModel();
 
-	const bookModel = new BookModel();
 
-	/*
+async function getAll() {
+
+	const result = await bookModel.getAll();
+	console.log(result);
+	
+
+}
+
+
+async function create() {
+
 	const title = readLine.question('Digite o título do livro: ');
 	const price = readLine.question('Digite o preço do livro: ');
 	const author = readLine.question('Digite o autor do livro: ');
@@ -17,20 +26,19 @@ const main = async () => {
 
 	const newBook: Book = { title, price, author, isbn }; 
 	
-	//const createdBook = await bookModel.create(newBook);
-	//console.log(createdBook);
-	*/
-	
-	
-	const result = await bookModel.getAll();
-	console.log(result);	
+	const createdBook = await bookModel.create(newBook);
+	console.log(createdBook);
+
+}
+
+const main = async () => {
+
+	//await create();
 		
+	await getAll();
 	
-	/*
-	const result = await connection.execute('SELECT * FROM books'); // testando
-  	const [rows] = result;
-  	console.log(rows);
-  	*/
+	process.exit();
+	
 }
 
 	main();
